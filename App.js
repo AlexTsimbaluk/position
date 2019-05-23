367c59fd623a9f1fe2aa7be84d4fbaab7880b44f
Alexandr Tsimbaluk <aleksandr.tsimbaluk@gmail.com>
2019-05-23 15:26:11 +0300

    кнопка

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, ScrollView, View, Text, Button} from 'react-native';
 import { LineChart, Grid } from 'react-native-svg-charts';
 
 
@@ -17,7 +17,7 @@ const Value = ({name, value}) => (
 const ChartSpeed = ({speed, time}) => (
   <LineChart
     style={{ height: 200 }}
-    data={ time }
+    data={ speed }
     svg={{ stroke: 'rgb(134, 65, 244)' }}
     contentInset={{ top: 20, bottom: 20 }}
   >
@@ -25,10 +25,9 @@ const ChartSpeed = ({speed, time}) => (
   </LineChart>
 )
 
-class LineChartExample extends React.PureComponent {
+class LineChartExample extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
 
     this.state = {
       speed: [],
@@ -36,7 +35,7 @@ class LineChartExample extends React.PureComponent {
     };
   }
 
-  
+
 
   componentDidMount() {
     // this.getAxis();
@@ -49,15 +48,10 @@ class LineChartExample extends React.PureComponent {
   render() {
     const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ];
 
-    // console.log(this.props.speed.map(el => (el + Math.random * 10)));
-    console.log(this.props.speed);
-    console.log(this.props.time);
-
     return (
-        // data={ this.props.speed.map(el => (el + Math.random * 10)) }
       <LineChart
         style={{ height: 200 }}
-        data={ this.props.time }
+        data={ this.props.speed }
         svg={{ stroke: 'rgb(134, 65, 244)' }}
         contentInset={{ top: 20, bottom: 20 }}
       >
@@ -99,42 +93,26 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.watchPosition();
+    // this.watchPosition();
   }
 
   componentWillUnmount() {
     this.watchID && this.stopWatchPosition();
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Value name="Широта: " value={this.state.location.coords.latitude} />
-        <Value name="Долгота: " value={this.state.location.coords.longitude} />
-        <Value name="Высота: " value={this.state.location.coords.altitude} />
-        <Value name="Точность: " value={this.state.location.coords.accuracy} />
-        <Value name="Точность высоты: " value={this.state.location.coords.altitudeAccuracy} />
-        <Value name="Направление: " value={this.state.location.coords.heading} />
-        <Value name="Скорость: " value={this.getSpeedInKm(this.state.location.coords.speed)} />
-        <Value name="Время: " value={this.state.location.timestamp} />
-
-        <LineChartExample speed={this.state.speed} time={this.state.time} />
-        <ChartSpeed speed={this.state.speed} time={this.state.time} />
-      </View>
-    );
-  }
-
   getPosition() {
     navigator.geolocation.getCurrentPosition(this.gpsWatchSuccess, this.gpsError, this.state.gpsOptions);
   }
 
-  watchPosition() {
+  watchPosition = () => {
     this.watchID = navigator.geolocation.watchPosition(this.gpsWatchSuccess, this.gpsError, this.state.gpsOptions);
   }
 
-  stopWatchPosition() {
-    navigator.geolocation.clearWatch(this.watchID);
-    this.setState({watchID: null});
+  stopWatchPosition = () => {
+    if(this.watchID) {
+      navigator.geolocation.clearWatch(this.watchID);
+      this.setState({watchID: null});
+    }
   }
 
   gpsWatchSuccess(position) {
@@ -148,9 +126,6 @@ export default class App extends Component {
       coords: crd,
       timestamp: this.dateFromTimestap(position.timestamp)
     }});
-
-    // console.log(crd.speed, this.dateFromTimestap(position.timestamp));
-    // this.setSpeedChart(this.getSpeedInKm(crd.speed), this.dateFromTimestap(position.timestamp));
 
     this._setState('speed', this.getSpeedInKm(crd.speed));
     this._setState('time', this.dateFromTimestap(position.timestamp));
@@ -166,18 +141,53 @@ export default class App extends Component {
     var hours = (d.getHours() < 10) ? ('0' + d.getHours()) : d.getHours();
     var min = (d.getMinutes() < 10) ? ('0' + d.getMinutes()) : d.getMinutes();
     var sec = (d.getSeconds() < 10) ? ('0' + d.getSeconds()) : d.getSeconds();
-    // return hours + ':' + min + ':' + sec;
-    return d.getHours() + d.getMinutes() + d.getSeconds();
+    return hours + ':' + min + ':' + sec;
   }
 
   getSpeedInKm(speedInM) {
     return speedInM * 3600 / 1000;
   }
 
-  setSpeedChart(speed, timestamp) {
-    let _speedChart = this.state.speedChart;
-    _speedChart.push({speed: speed, timestamp: timestamp});
-    this.setState({speedChart: _speedChart});
+  render() {
+          // !!(this.state.watchID !== null)
+    return (
+      <ScrollView style={styles.container}>
+        {
+          !!(this.state.speed.length)
+          ?
+          <View>
+            <Value name="Широта: " value={this.state.location.coords.latitude} />
+            <Value name="Долгота: " value={this.state.location.coords.longitude} />
+            <Value name="Высота: " value={this.state.location.coords.altitude} />
+            <Value name="Точность: " value={this.state.location.coords.accuracy} />
+            <Value name="Точность высоты: " value={this.state.location.coords.altitudeAccuracy} />
+            <Value name="Направление: " value={this.state.location.coords.heading} />
+            <Value name="Скорость: " value={this.getSpeedInKm(this.state.location.coords.speed)} />
+            <Value name="Время: " value={this.state.location.timestamp} />
+
+            <LineChartExample speed={this.state.speed} time={this.state.time} />
+            <ChartSpeed speed={this.state.speed} time={this.state.time} />
+
+            <Button
+              onPress={this.stopWatchPosition}
+              title="Stop watch position"
+              color="#ff0000"
+              style={styles.button}
+            />
+          </View>
+          :
+          <View>
+            <Button
+              onPress={this.watchPosition}
+              title="Watch position"
+              color="#00ff00"
+              style={styles.button}
+            />
+          </View>
+        }
+
+      </ScrollView>
+    );
   }
 }
 
@@ -209,5 +219,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingRight: 10,
     // marginLeft: 10,
+  },
+  button: {
+    flexGrow: 1
   }
 });
